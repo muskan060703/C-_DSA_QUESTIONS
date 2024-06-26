fe814c37de9f1c42284d13f148c2e5f57ba577b2
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-06-26 09:29:52 +0000

    change

---
+++ 02_BIT_MAGIC/Two numbers with odd occurrences.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+    public:
+    vector<long long int> twoOddNum(long long int Arr[], long long int N)  
+    {
+        // code here
+        vector<long long int>ans;
+        long long int x=Arr[0];
+        for(long long int i=1;i<N;i++)
+        {
+            x=x^Arr[i];
+        }
+        long long int k=(x&(~(x-1)));
+        long long int res1=0;
+        long long int res2=0;
+        for(long long int i=0;i<N;i++)
+        {
+            if((Arr[i]&k)!=0)
+            {
+                res1=res1^Arr[i];
+            }
+            else
+            {
+                res2=res2^Arr[i];
+            }
+        }
+        ans.push_back(res1);
+        ans.push_back(res2);
+        sort(ans.begin(),ans.end(),greater<long long int>());
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int N;
+        cin>>N;
+        long long int Arr[N];
+        for(long int i = 0;i < N;i++)
+        cin>>Arr[i];
+        Solution ob;
+        vector<long long int>ans=ob.twoOddNum(Arr,N);
+        for(long long int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
