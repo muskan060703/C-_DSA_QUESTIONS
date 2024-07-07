1cde5e7e6741b2cc2004b32965cac39ce0c09d62
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-07-07 08:26:37 +0000

    bit

---
+++ 02_BIT_MAGIC/Bit Difference.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        
+        // Your logic here
+        if(a == b) return -1;
+        
+        int temp = a^b;
+        int cnt = 0;
+        while(temp > 0)
+        {
+            if(temp & 1 != 0 ) cnt++;
+            temp = temp / 2;
+        }
+        return cnt ;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;// input the testcases
+	while(t--) //while testcases exist
+	{
+		int a,b;
+		cin>>a>>b; //input a and b
+
+        Solution ob;
+		cout<<ob.countBitsFlip(a, b)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
---
+++ 02_BIT_MAGIC/Rightmost different bit.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        if(m == n) return -1;
+        
+        int temp = m^n;
+        int cnt = 1;
+        while(temp > 0)
+        {
+            if(temp & 1 != 0 ) return cnt ;
+            temp = temp / 2;
+            cnt++;
+        }
+        return cnt ;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Ends
